5b1d3d18b7c150305e70042dd460fdbdc5afaff3
Neil Adams <neiladams@Neils-MacBook-Air-2.local>
2026-09-07 17:33:37 +0300

    Use installed Python when creating development environment

--- dev-start.sh
+++ dev-start.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -e
 cd "$(dirname "$0")"
-[ -x .venv/bin/python ] || /opt/homebrew/bin/python3.11 -m venv .venv
+[ -x .venv/bin/python ] || /opt/homebrew/bin/python3 -m venv .venv
 source .venv/bin/activate
 exec waitress-serve --listen=127.0.0.1:${PORT:-8080} app:app
 
